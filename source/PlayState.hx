ef39fbf2ff169783a1670fc43a807a123b3559e8
Cyklus <alvaro11chulo@gmail.com>
2025-06-20 18:12:01 -0300

    chart update and fix

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1986,17 +1986,16 @@ class PlayState extends MusicBeatState
 
 			// FlxG.switchState(new GameOverState(boyfriend.getScreenPosition().x, boyfriend.getScreenPosition().y));
 		}
-
-		if (unspawnNotes[0] != null)
-		{
-			if (unspawnNotes[0].strumTime - Conductor.songPosition < (SONG.song.toLowerCase() == 'unfairness' ? 15000 : 1500))
-			{
-				var dunceNote:Note = unspawnNotes[0];
-				notes.add(dunceNote);
-				dunceNote.finishedGenerating = true;
-
-				var index:Int = unspawnNotes.indexOf(dunceNote);
-				unspawnNotes.splice(index, 1);
+		if (unspawnNotes.length > 0) {
+			var thresholdTime = Conductor.songPosition + 600;
+			var i = unspawnNotes.length - 1; // start from the end
+		
+			while (i >= 0) {
+				if (unspawnNotes[i].strumTime < thresholdTime) {
+					notes.add(unspawnNotes[i]);
+					unspawnNotes.splice(i, 1);
+				}
+				i--;
 			}
 		}
 
